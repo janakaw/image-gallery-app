74dacce6a183fffcded45c704bdacf1197587fa0
Janaka Seneviratne <jseneviratne@tritium.com.au>
2021-01-02 13:05:22 +1000

    website

--- image_gallery/website/static/js/viewer_wrap.js
+++ image_gallery/website/static/js/viewer_wrap.js
@@ -1,31 +1,21 @@
 
-
-//
 let galleries = []
 
-function testgal(image_id) {
+function show_project(project_id) {
     let gallery = null
     try {
         galleries.forEach(function (item, index, array){
-           if (item["id"] == image_id) {
+           if (item["id"] == project_id) {
                gallery = item["gal"];
                return
            }
         });
         if (gallery == null) {
-            gallery = new Viewer(document.getElementById(image_id + "_1"), {
-                //container: document.getElementById('portfolio'),
-            });
-            galleries.push({"id": image_id, "gal": gallery })
+            gallery = new Viewer(document.getElementById(project_id + "_1"));
+            galleries.push({"id": project_id, "gal": gallery })
         }
-        console.log("teest");
-        console.log(image_id);
         gallery.show();
-        gallery.stop();
     } catch (err) {
         console.log(err.message);
     }
-
 }
-
-// const gallery = new Viewer(document.getElementById('images'));
